81d6fcfcb103c8bc9bbbcf68b535f22d29ad3044
CMENDEZ <33556182+c-mendez@users.noreply.github.com>
2024-01-23 21:05:03 -0600

    feat:Primer commit
    Vr. 0.00
    
    23/01/2024

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('product')->group(function () {
+    Route::get('/', function () {
+        return "Todos";
+    });
+
+    Route::get('/{id}', function ($id) {
+        return "Unico ".$id;
+    });
+
+    Route::put('/{id}', function ($id) {
+        return "Unico actualizar ".$id;
+    });
+
+    Route::post('/', function () {
+        return "Creado";
+    });
+});
